a2304a7c12dbe986057630bab50ca8733f79b886
Dan Kotovski <dan@appdome.com>
2018-01-17 16:38:54 +0200

    Add new script to sign all executables to run on IOS

---
+++ sign_all_ios.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+IDENTITY=""
+
+if [ $# -eq 1 ] && [ ! -z "$1" ]; then
+  IDENTITY="$1"
+else
+  security find-identity -v -p codesigning
+  echo "Supply identity as parameter"
+  exit
+fi
+
+
+find pan testcases/kernel/syscalls  -perm +111 -type f \
+-not -name "*.sh" -not -name "*.cgi" -not -name "*.cstemp" \
+-exec codesign -s "iPhone Developer: Dan Kotovski (G3WB73Q54Q)" {} \;
